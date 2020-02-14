af7ad9e5d5f1d495ebc08161fdcaf42348b4d9ed
Malcom Fetus <sora.vista@gmail.com>
2020-02-14 07:52:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
+require "pry"
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -11,3 +12,18 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+  ##binding.pry 
+  if phrase == phrase.upcase 
+     return "I LOVE YOU TOO PUMPKIN!"
+  
+  elsif !phrase == !phrase.upcase
+   return "HUH?! SPEAK UP, SONNY!"
+      
+  else phrase
+   return "NO, NOT SINCE 1938!"
+  end 
+end
+
+
