9631001d7585e95cd51c9c58d163ef97623f284c
Malcom Fetus <sora.vista@gmail.com>
2020-02-14 07:55:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase 
      return 'I LOVE YOU TOO PUMPKIN!'
   
-  elsif !phrase == !phrase.upcase
+  elsif phrase != phrase.upcase
    return "HUH?! SPEAK UP, SONNY!"
       
   else
