8173573afd1071b8bf0fc8aa6498b1b73fdd2f09
Malcom Fetus <sora.vista@gmail.com>
2020-02-14 07:52:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,8 +21,8 @@ def speak_to_grandma(phrase)
   elsif !phrase == !phrase.upcase
    return "HUH?! SPEAK UP, SONNY!"
       
-  else phrase
-   return "NO, NOT SINCE 1938!"
+  else
+  "NO, NOT SINCE 1938!"
   end 
 end
 
